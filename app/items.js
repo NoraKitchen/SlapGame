531a5938b32ed9f5e80c4874dc660a41a12f7472
NoraKitchen <narwhaltortellini@gmail.com>
2016-07-04 16:23:45 -0600

    item cards complete

--- app/items.js
+++ app/items.js
@@ -4,11 +4,13 @@ var Item = function (name, modifier, imagePath, description) {
     this.modifier = modifier;
     this.imagePath = imagePath;
     this.description = description;
-    this.panelHTML = '<div class="col-xs-4 panel-default"><div class="panel-heading">' + name + '</div><div class="panel-body"><img class="item-img img-responsive" src="' + imagePath + '"></div><div class="panel-footer">' + description + '</div></div>'
+    // this.panelHTML =  '<div class="col-xs-4 panel-default border"><div class="panel-heading item-panel">'+ name + '</div><div class="panel-body"><img class="item-img img-responsive" src="' + imagePath + '"></div><div class="panel-footer item-panel">' + description + '</div></div>';
+    this.panelHTML =  '<div class="col-xs-4"><div class="panel-default border"><div class="panel-heading item-panel">'+ name + '</div><div class="panel-body"><img class="item-img img-responsive" src="' + imagePath + '"></div><div class="panel-footer item-panel">' + description + '</div></div></div>';
+
 };
 
 var availableItems = {
-    box: new Item("Empty Cardboard Box", 0.2, "media/box.jpg", "Empty, yet strangely alluring."),
-    liveMouse: new Item("Live Mouse", 0.3, "media/mouse.jpg", "Alive and wrigling."),
-    catnip: new Item("Catnip", 0.4, "media/catnip.jpg", "Sweet, sweet kitty drugs.")
+    box: new Item("EMPTY BOX", 0.2, "media/box.jpg", "Empty, yet strangely alluring."),
+    liveMouse: new Item("LIVE MOUSE", 0.3, "media/mouse.jpg", "Just wants to be friends."),
+    catnip: new Item("CATNIP", 0.4, "media/catnip.jpg", "Sweet, sweet kitty drugs.")
 }
